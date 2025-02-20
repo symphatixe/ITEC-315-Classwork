804d7df312512758e72cddc2720050ff7d298774
vadim <symphatixe@gmail.com>
2025-02-20 15:25:23 -0500

    Removal of old files
    Removing the folders to add the apps that we made in class as submodules so they are stored in separate repositories.

--- quiz_app/lib/models/quiz_question.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class QuizQuestion extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    throw UnimplementedError();
-  }
-
-}
